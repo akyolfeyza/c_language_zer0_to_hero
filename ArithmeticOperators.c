56f4181510a874a636532449aa84bd5143ed0653
1Bartu_Paçal3 <118813470+BartuPacall@users.noreply.github.com>
2022-12-10 12:07:14 +0300

    Update ArithmeticOperators.c

--- ArithmeticOperators.c
+++ ArithmeticOperators.c
@@ -1,23 +1,19 @@
-
-
 // Eksiklikleri gider, hatayi coz. 
 
 #include <stdio.h>
 int main()
 {
-    int a = 9, 
-        b = 4, 
-        c;
+    int a = 9, b = 4;
     
     c = a+b;
-    printf("a+b = %d \n",c);
+    printf("a+b = %d \n",a+b);
     c = a-b;
-    printf("a-b = %d \n",c);
+    printf("a-b = %d \n",a-b);
     c = 
-    printf("a*b = %d \n",c);
+    printf("a*b = %d \n",a*b);
     c = a/b;
-    printf("a/b = %d \n",c);
-    c = 
-    printf("Remainder when a divided by b = %d \n",c);
+    printf("a/b = %d \n",a/b);
+    printf("Remainder when a divided by b = %d \n",a%b);
     
     return 0;
+}
