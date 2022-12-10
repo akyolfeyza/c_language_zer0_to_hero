2717fa6b587f5e210a4534d6564a4fbb8ece80a6
MelisaOnal <119419249+MelisaOnal@users.noreply.github.com>
2022-12-10 20:50:35 +0300

    Create favori.c

---
+++ Cozumler/favori.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(){
+    int sayi;
+    printf("favori sayinizi giriniz\n");
+    scanf("%d",&sayi);
+    printf("favori sayiniz %d.",sayi);
+    return 0 ;
+
+
+
+}
