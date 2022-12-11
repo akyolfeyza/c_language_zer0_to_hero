30cf999b22a66e90fc8def06e22a8110faa8492f
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-11 19:16:16 +0300

    Create yourNameYourGrade.c

---
+++ yourNameYourGrade.c
@@ -0,0 +1,20 @@
+//name and grade 
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+int main() {
+    int name[20];
+    char grade;
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Enter your grade: ");
+    scanf("%d", &grade);
+    if (strcmp(name, name) == 0 && grade > 50) {
+        printf("Your name is %s and your grade is %d and Congrats you passed", name, grade);
+    } else {
+        printf("Your name is %s and your grade is %d, you failed; no worries try again.", name, grade);
+    }
+    return 0;
+}
