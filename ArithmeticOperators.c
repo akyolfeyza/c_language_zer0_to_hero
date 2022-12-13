26a2c60f7bf88197642f1c5c7e28bb2ed87c85e8
akyolfeyza <120449870+akyolfeyza@users.noreply.github.com>
2022-12-13 15:07:20 +0300

    Feyza Akyol
    @themanoftalent

--- ArithmeticOperators.c
+++ ArithmeticOperators.c
@@ -3,21 +3,23 @@
 // Eksiklikleri gider, hatayi coz. 
 
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
-    int a = 9, 
-        b = 4, 
-        c;
+    int a = 9; 
+    int b = 4; 
+    int c;
     
     c = a+b;
     printf("a+b = %d \n",c);
     c = a-b;
     printf("a-b = %d \n",c);
-    c = 
+    c = a*b;
     printf("a*b = %d \n",c);
     c = a/b;
     printf("a/b = %d \n",c);
-    c = 
+    c = a%b;
     printf("Remainder when a divided by b = %d \n",c);
     
     return 0;
+}
